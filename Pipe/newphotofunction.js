afbc002541781e0ac1782dc5a4d7a0e30f7bc9e1
pipefake <jfjimenezsalazar@gmail.com>
2022-04-09 23:32:58 -0500

    Required

--- Pipe/newphotofunction.js
+++ Pipe/newphotofunction.js
@@ -9,13 +9,15 @@ function leerLocalStorageArtist(){
             const contArtista = document.getElementById('contArtista');
 	        let HTMLString = `
             <div>
-            <input id="nombreedit_${i}" name="nombreedit"  type="text" value="${listaArtista[i].nombre}">
-            <input id="listaedit_${i}" name="nombreedit"  type="text" value="${listaArtista[i].lista}">
+            <form action="#">
+            <input id="nombreedit_${i}" name="nombreedit"  type="text" value="${listaArtista[i].nombre}" required>
+            <input id="listaedit_${i}" name="nombreedit"  type="text" value="${listaArtista[i].lista}" required>
             <img src="${listaArtista[i].img}">
-            <input type="file" id="imgedit_${i}" name="fichero"  accept=".jpg, .jpeg, .png"  />
-            <input id="descedit_${i}" name="nombreedit"  type="text" value="${listaArtista[i].desc}">
+            <input type="file" id="imgedit_${i}" name="fichero"  accept=".jpg, .jpeg, .png"  value="${listaArtista[i].img}" required>
+            <input id="descedit_${i}" name="nombreedit"  type="text" value="${listaArtista[i].desc}" required>
             <button onclick="delet(${i})"> Eliminar </button>
             <button onclick="edit(${i})"> Editar </button>
+            </form>
             </div>
             `;
 	        contArtista.innerHTML += HTMLString;
